c37683e1ed6948a681b4a9ada502a4b4dc88b634
bookgvi <bookgvi@gmail.com>
2019-08-02 11:56:53 +0300

    Learning Vue single file component with webpack

---
+++ component/singleFile/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="hello">
+    <span>{{msg}}</span>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data: function(){
+        return {
+            msg: 'Holly Vue!'
+        }
+    }, 
+}
+</script>
+
+<style>
+.hello{
+    font-family: Verdana;
+    color: navy;
+}
+</style>
+
+
