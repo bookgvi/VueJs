2d6c1a67a512d8b4af6bcff7a8afcfd5dfb222f6
bookgvi <bookgvi@gmail.com>
2019-08-08 00:46:39 +0300

    Create an 'mutation'

--- Learning/Vuex/example/src/Components/App.vue
+++ Learning/Vuex/example/src/Components/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
-    Holly Vuex!
+    Holly Vuex! {{$store.state.counter}}
   </div>
 </template>
+
+<script>
+  export default {
+      data() {
+          return {
+              counter: this.$store.state.counter,
+          }
+      },
+      computed: {
+          increment() {
+              return this.$store.commit('INCREMENT')
+          },
+      },
+  }
+</script>
--- Learning/Vuex/example/src/main.js
+++ Learning/Vuex/example/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './Components/App.vue'
+import store from './store'
+
+store.commit('increment');
 
 new Vue({
   el: '#app',
+  store,
   render: h=>h(App),
 });
