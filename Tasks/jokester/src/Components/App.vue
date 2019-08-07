3baac9a0e2f429eee36443a8d87752e3cd42a6d8
bookgvi <bookgvi@gmail.com>
2019-08-08 02:57:13 +0300

    Learning 'actions'

--- Tasks/jokester/src/Components/App.vue
+++ Tasks/jokester/src/Components/App.vue
@@ -1,5 +1,18 @@
 <template>
     <div id="app">
-        Get jokes? {{$store.state}}
+      <h3>Get jokes?</h3>
+      <button class="btn btn-primary" @click='initJokes'>Get Data</button>
+      <div>
+        {{$store.state}}
+      </div>
     </div>
 </template>
+
+<script>
+  import {mapActions} from 'vuex'
+  export default {
+      methods: mapActions([
+          'initJokes'
+      ])
+  }
+</script>
--- Tasks/jokester/src/store/index.js
+++ Tasks/jokester/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import {mutations} from './mutations'
+import * as actions from './actions'
+
 Vue.use(Vuex);
 
 const state = {
@@ -10,5 +12,6 @@ const state = {
 
 export default new Vuex.Store({
   state,
-  mutations
+  mutations,
+  actions,
 });
