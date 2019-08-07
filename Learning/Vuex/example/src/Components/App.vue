6a8042f36c91c191c7f2ef7909574603851fea83
bookgvi <bookgvi@gmail.com>
2019-08-08 01:23:08 +0300

    Learning an 'mutations'

--- Learning/Vuex/example/src/Components/App.vue
+++ Learning/Vuex/example/src/Components/App.vue
@@ -1,20 +1,10 @@
 <template>
   <div id="app">
     Holly Vuex! {{$store.state.counter}}
+    <button @click="$store.commit('INCREMENT','+')">+</button>
+    <button @click="$store.commit('INCREMENT', '-')">-</button>
+    <button @click="$store.commit('INCREMENT', -2)">-2</button>
+    <button @click="$store.commit('INCREMENT', 3)">+3</button>
+    <button @click="$store.commit('INCREMENT', 0-$store.state.counter)">reset</button>
   </div>
 </template>
-
-<script>
-  export default {
-      data() {
-          return {
-              counter: this.$store.state.counter,
-          }
-      },
-      computed: {
-          increment() {
-              return this.$store.commit('INCREMENT')
-          },
-      },
-  }
-</script>
--- Learning/Vuex/example/src/main.js
+++ Learning/Vuex/example/src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './Components/App.vue'
 import store from './store'
 
-store.commit('increment');
-
 new Vue({
   el: '#app',
   store,
