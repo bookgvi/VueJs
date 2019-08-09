b7c3c0536d1cfbbc13583b18b052aa8c6c63955e
bookgvi <bookgvi@gmail.com>
2019-08-09 21:06:08 +0300

    Just a test App

---
+++ Tasks/glitchtext/src/Components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>This is an example text</h1>
+    <glitch-text />
+    <button>Get a text</button>
+  </div>
+</template>
+
+<script>
+  import Glitchtext from './Glitchtext'
+  export default{
+    components: {
+        'glitch-text': Glitchtext,
+    },
+  }
+</script>
+
+<style scope>
+  #app{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    margin-top: 3rem;
+    text-align-last: center;
+  }
+</style>
---
+++ Tasks/glitchtext/src/Components/Glitchtext.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    {{text}}
+    <button @click="glitchMethod">Press me</button>
+  </div>
+</template>
+
+<script>
+  export default{
+      data(){
+          return {
+              targetText: 'This text will be glitched',
+              glitch: '!@#$%^&*()_+',
+              finalText: '',
+              speed: 100,
+          }
+      },
+      computed: {
+          text(){
+              return this.finalText;
+          },
+      },
+      methods: {
+          glitchMethod(){
+                  let i=0;
+                  self = this;
+                  setTimeout(function sChar(){
+                      if (i >= self.targetText.length) return self.finalText;
+                      self.finalText += self.targetText[i];
+                      i++;
+                      setTimeout(sChar, self.speed);
+                  }, self.speed);
+          },
+      },
+  }
+</script>
