b71ba5c4d90c129ef07e60254e30cf91531c5a3c
bookgvi <bookgvi@gmail.com>
2019-08-11 15:04:00 +0300

    Сайт с информацией о фильмах (пока в разаработке)

--- Tasks/nokino/src/Components/App.vue
+++ Tasks/nokino/src/Components/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app" class="container">
-    <h2 class="mt-9">Holly, Vue</h2>
+    <h2 class="mt-9">{{filmTitle}}</h2>
     <hr>
     <div class="">
-      <button class="btn btn-dark mb-2" @click="fetchFilmsInfo(-1)" v-if="previousFilmTitle">{{previousFilmTitle}}</button>
+      <button class="btn btn-dark mb-2 mr-2" @click="fetchFilmsInfo(-1)" v-if="previousFilmTitle">{{previousFilmTitle}}</button>
       <button class="btn btn-dark mb-2" @click="fetchFilmsInfo(1)" v-if="nextFilmTitle">{{nextFilmTitle}}</button>
       <div class="">
         <img :src='filmPoster' alt="" >
@@ -18,6 +18,7 @@
           return {
               baseImgSrc: 'https://image.tmdb.org/t/p/w500',
               filmInfo: '',
+              filmTitle: '',
               nextFilmTitle: '',
               previousFilmTitle: '',
               filmPoster: '',
@@ -29,7 +30,6 @@
           },
       methods: {
           fetchFilmsInfo(counter){
-              console.log(counter, this.filmCounter)
               fetch('https://api.themoviedb.org/3/discover/movie?api_key=382fdc20a456036a67a5e56974cb5016',{
                   method: 'GET',
                   // mode: 'no-cors',
@@ -38,10 +38,11 @@
                   .then(json => this.filmInfo = json.results)
                   .then(()=> {
                       this.filmPoster = this.baseImgSrc + this.filmInfo[this.filmCounter].poster_path;
+                      this.filmTitle = this.filmInfo[this.filmCounter].title;
                       this.nextFilmTitle = this.filmInfo[this.filmCounter+1].title;
                       this.previousFilmTitle = this.filmInfo[this.filmCounter-1].title;
                   })
-                  .catch(()=>console.dir('...Catched load img error'));
+                  .catch(()=>console.error('...Catched load img error'));
               this.filmCounter+=counter;
           }
       }
