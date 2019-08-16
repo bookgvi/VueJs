7af76bd133d72f649c633e989a2345450816fff3
bookgvi <bookgvi@gmail.com>
2019-08-16 05:57:47 +0300

    vue-yandex-maps 1) Connected 2) MapMarker v-on:click

--- Tasks/ya_maps/src/App.vue
+++ Tasks/ya_maps/src/App.vue
@@ -5,8 +5,15 @@
       :coords="moscow.center"
       :zoom="moscow.zoom"
       :map-type="mapsGeneral.mapType[0]"
-      style="width: 600px; height: 600px;"
-    ></yandex-map>
+      style="width: 1024px; height: 680px;"
+      @click="hMapClick"
+    >
+      <ymap-marker
+        marker-id="111"
+        hint-content="some hint"
+        :coords="marker.coords"
+      ></ymap-marker>
+    </yandex-map>
   </div>
 </template>
 
@@ -19,8 +26,16 @@ export default {
     },
     mapsGeneral: {
       mapType: ['map', 'satellite', 'hybrid']
+    },
+    marker: {
+      coords: []
     }
-  })
+  }),
+  methods: {
+    hMapClick (e) {
+      this.marker.coords = e.get('coords')
+    }
+  }
 }
 </script>
 
