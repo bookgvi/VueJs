53369f9cc71341a3117af22154c3734b78c93916
bookgvi <bookgvi@gmail.com>
2019-08-17 04:14:36 +0300

    beforeEach

--- Learning/Routing/named-view/src/main.js
+++ Learning/Routing/named-view/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './components/App'
 import { router } from './router'
 
+router.beforeEach((to, from ,next) => {
+  console.dir(to)
+  console.log ('"',from.fullPath,'" => "',to.fullPath,'"')
+  next()
+})
+
 new Vue({
   el: '#app',
   router,
