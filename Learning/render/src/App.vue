f84c792584033381b0e7b44814a1fd1b7faaad5c
bookgvi <bookgvi@gmail.com>
2019-08-22 12:46:48 +0300

    Learning render functions

---
+++ Learning/render/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import NewComponent from './components/NewComponent'
+export default {
+  components: {
+    'my-comp': NewComponent
+  },
+  render: h => h (
+      'div', [
+        'Holly, Vue',
+        h('my-comp', { props: { someProp: 'wat' } })
+      ],
+    )
+}
+</script>
---
+++ Learning/render/src/components/NewComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    someProp: String
+  },
+  data: () => ({
+    testVar: 'This is the test'
+  }),
+  render: function (h) {
+    return h('div', [
+      h('h2', this.someProp),
+      Array.apply(null, {length: 20})
+        .map(() => {
+          return h('p','Vue')
+        })
+    ])
+  },
+  created() {
+    console.log(this.someProp)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
