310155edf5433b02458441ba28cf812220be61be
bookgvi <bookgvi@gmail.com>
2019-09-01 14:02:07 +0300

    http-rest-axios

---
+++ Learning/productapp/restData.js
@@ -0,0 +1,11 @@
+module.exports = function () {
+  var data = {
+    products: [
+      { id: 1, name: "Kayak", price: 275 },
+      { id: 2, name: "Lifejacket", price: 48.95 },
+      { id: 3, name: "Soccer Ball", price: 19.50 },
+      { id: 4, name: "Corner Flags", price: 39.95 },
+      { id: 5, name: "Stadium", price: 79500 }]
+  }
+  return data
+}
--- Learning/productapp/src/components/ProductDisplay.vue
+++ Learning/productapp/src/components/ProductDisplay.vue
@@ -28,12 +28,7 @@
 <script>
 export default {
   data: () => ({
-      products: [
-        { id: 1, name: "Kayak", price: 275 },
-        { id: 2, name: "Lifejacket", price: 48.95 },
-        { id: 3, name: "Soccer Ball", price: 19.50 },
-        { id: 4, name: "Corner Flags", price: 39.95 },
-        { id: 5, name: "Stadium", price: 79500 }]
+      products: []
   }),
   inject: ['eventBus'],
   filters: {
