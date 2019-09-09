fa0f718ecbe7502faa86b700a909764c2a3faaf3
bookgvi <bookgvi@gmail.com>
2019-09-09 22:33:47 +0300

    Fix conflict error (json attrib "updated" with lifecycle hook (updated))

--- Learning/bitcoinPrices/src/pages/Index.vue
+++ Learning/bitcoinPrices/src/pages/Index.vue
@@ -2,12 +2,12 @@
   <q-page>
    <div class="row q-pa-xl">
      <q-card style="width: 290px;" class="text-center q-pa-lg">
-         <h6 class="q-ma-none">{{ bitcoin.time.updated }}</h6>
+         <h6 class="q-ma-none">BTS/USD</h6>
        1BTS = ${{ bitcoin.bpi.USD.rate }} {{ bitcoin.bpi.USD.code }}
      </q-card>
      <q-space></q-space>
      <q-card style="width: 290px;" class="text-center q-pa-lg">
-         <h6 class="q-ma-none">{{ bitcoin.time.updated }}</h6>
+         <h6 class="q-ma-none">BTS/EUR</h6>
        1BTS = ${{ bitcoin.bpi.EUR.rate }} {{ bitcoin.bpi.EUR.code }}
      </q-card>
    </div>
@@ -20,9 +20,11 @@
 <script>
 export default {
   name: 'PageIndex',
-  data: () => ({
-    bitcoin: ''
-  }),
+  data () {
+    return {
+      bitcoin: ''
+    }
+  },
   async created () {
     this.bitcoin = await this.$http.get().then(resp => resp.data)
   }
