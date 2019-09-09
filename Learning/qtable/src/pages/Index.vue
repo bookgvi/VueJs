27eceff26db5920db1a7f7123f16bc98dc579490
bookgvi <bookgvi@gmail.com>
2019-09-10 00:38:47 +0300

    QTable simple test (repeat for knowledge)

---
+++ Learning/qtable/src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-table
+      :columns="columns"
+      :data="data"
+    >
+    </q-table>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data: () => ({
+    columns: [
+      { name: 'id', label: 'number', field: 'id' },
+      { name: 'name', label: 'name', field: 'name' }
+    ],
+    data: [{
+      id: 1,
+      name: 'WTF'
+    }]
+  })
+}
+</script>
