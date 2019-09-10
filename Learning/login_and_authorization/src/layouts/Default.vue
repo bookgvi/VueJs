42aa283b33c4b2fd1f859a8e3be10a3bf405a7c1
bookgvi <bookgvi@gmail.com>
2019-09-10 23:14:19 +0300

    Starting manipulation. Empty project

---
+++ Learning/login_and_authorization/src/layouts/Default.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  q-layout.lHh Lpr lFf
+    q-page-container
+      router-view
+</template>
+
+<script>
+export default {
+  name: 'MyLayout'
+}
+</script>
+
+<style>
+</style>
