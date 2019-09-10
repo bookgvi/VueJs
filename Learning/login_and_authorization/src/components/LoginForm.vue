b9c415435ed4ecdd8cb900c09945e27488222637
bookgvi <bookgvi@gmail.com>
2019-09-11 00:11:04 +0300

    Starting manipulation. Empty project

--- Learning/login_and_authorization/src/components/LoginForm.vue
+++ Learning/login_and_authorization/src/components/LoginForm.vue
@@ -1,12 +1,41 @@
 <template lang="pug">
   q-form
-    q-input
+    q-input(
+      v-model="login"
+      placeholder="Login"
+      label="Login"
+      clearable
+      outlined
+      dense
+    )
+    q-input.q-mt-xs(
+      v-model="password"
+      type="password"
+      placeholder="Password"
+      label="Password"
+      clearable
+      outlined
+      dense
+    )
+    q-btn.q-mt-sm.q-ml-sm(@click="submit" color="primary") Login
+    q-btn.q-mt-sm.q-ml-sm(@click="cancel") Cancel
 </template>
 
 <script>
-  export default {
-    name: "LoginForm"
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    login: '',
+    password: ''
+  }),
+  methods: {
+    submit () {},
+    cancel () {
+      this.login = ''
+      this.password = ''
+    }
   }
+}
 </script>
 
 <style scoped>
--- Learning/login_and_authorization/src/pages/Index.vue
+++ Learning/login_and_authorization/src/pages/Index.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
-  q-page.flex.flex-center
+  q-page.flex.q-pa-xl
+    login-form
 </template>
+
+<script>
+import LoginForm from '../components/LoginForm'
+export default {
+  name: 'PageIndex',
+  components: { LoginForm },
+  modules: {
+    LoginForm
+  }
+}
+</script>
 
 <style>
 </style>
-
-<script>
-export default {
-  name: 'PageIndex'
-}
-</script>
