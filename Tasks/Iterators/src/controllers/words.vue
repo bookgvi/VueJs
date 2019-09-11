bbe1807c166f9e2ec900324c8bbf57c343b28aa4
bookgvi <bookgvi@gmail.com>
2019-09-11 22:55:32 +0300

    Finished

--- Tasks/Iterators/src/controllers/words.vue
+++ Tasks/Iterators/src/controllers/words.vue
@@ -1,4 +1,10 @@
 <script>
 export default {
+  wordsCount (str) {
+    return str.split(' ').filter(item => item.length > 0)
+  },
+  getWords (str) {
+    return str[Symbol.iterator]()
+  }
 }
 </script>
--- Tasks/Iterators/src/pages/Index.vue
+++ Tasks/Iterators/src/pages/Index.vue
@@ -1,11 +1,36 @@
 <template lang="pug">
-  q-page.flex.flex-center
+  q-page.q-ma-xl
+    .row
+      q-input.q-mr-sm(v-model="str" outlined dense style="width: 500px")
+        template(#append)
+          div {{ wCount }}
+    .row
+      q-chip.color-primary Words count = {{ wCount }}
+      q-chip.color-primary {{ wordIterator }}
 </template>
 
 <style>
 </style>
 
 <script>
+import words from '../controllers/words'
 export default {
+  data: () => ({
+    str: '',
+    wordsInStr: ''
+  }),
+  computed: {
+    wCount () {
+      return words.wordsCount(this.str).length
+    },
+    wordIterator () {
+      let wwordsInStr = ''
+      let iterator = words.getWords(words.wordsCount(this.str))
+      for (let word of iterator) {
+        wwordsInStr += word + ' '
+      }
+      return wwordsInStr
+    }
+  }
 }
 </script>
