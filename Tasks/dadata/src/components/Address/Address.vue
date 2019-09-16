f555c2d873a5e1049b09d9a12acafe2b543d7aed
bookgvi <bookgvi@gmail.com>
2019-09-16 20:43:12 +0300

    Dadata: Start template

---
+++ Tasks/dadata/src/components/Address/Address.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .address-class
+    .row.q-pb-lg
+      .col
+        span Input field
+
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Tasks/dadata/src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  q-page
+    .row.justify-center.q-pt-xl
+      .col-8
+        Address
+</template>
+
+<style>
+</style>
+
+<script>
+import Address from '../components/Address/Address'
+export default {
+  name: 'PageIndex',
+  components: { Address }
+}
+</script>
