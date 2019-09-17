ba6fcfa08b46969098bef8179bf4b6b94d2dd012
bookgvi <bookgvi@gmail.com>
2019-09-17 23:44:22 +0300

    Dadata: Запрос на координаты map yandex api

---
+++ Tasks/dadata/src/components/VueDadata/VueDadata.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .vuedadata
+    .row.q-pb-lg
+      h5 Vue Components
+    .row.q-pb-lg
+        VueSuggestion(
+          :model.sync="city"
+          :coordinates.sync="coordinates"
+          :placeholder="'Начните вводить'"
+          class="form-control"
+          :options="suggestionOptions"
+        )
+
+</template>
+
+<script>
+import VueSuggestion from 'vue-suggestions'
+export default {
+  components: {
+    VueSuggestion
+  },
+  data () {
+    return {
+      city: '',
+      coordinates: {
+        latitude: '',
+        longitude: ''
+      },
+      suggestionOptions: {
+        // @see https://confluence.hflabs.ru/pages/viewpage.action?pageId=207454318
+        token: 'daa0567fa0fb73ae73ae7e1e389dfefe52ef35b9',
+        type: 'ADDRESS',
+        scrollOnFocus: false,
+        triggerSelectOnBlur: false,
+        triggerSelectOnEnter: false,
+        addon: 'none',
+        // @see https://confluence.hflabs.ru/pages/viewpage.action?pageId=207454320
+        onSelect (suggestion) {
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Tasks/dadata/src/pages/VueDadata.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  q-page
+    .row.justify-center.q-pt-xl
+      .col-8
+        Vuedadata
+</template>
+
+<style>
+</style>
+
+<script>
+import Vuedadata from '../components/VueDadata/VueDadata'
+export default {
+  name: 'VueDadataPage',
+  components: { Vuedadata }
+}
+</script>
