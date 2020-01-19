d0eb7c7cc601a8733b2f17cc562be540466d4f2d
book <bookgvi@gmail.com>
2020-01-19 12:49:48 +0300

    Yandex map

--- ymaps/src/App.vue
+++ ymaps/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ymap></ymap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ymap from './components/ymap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ymap
   }
 }
 </script>
---
+++ ymaps/src/components/ymap.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <yandex-map
+          :settings="settings"
+          map-type="map"
+          zoom=10
+          :coords="coords"
+          style="width: 800px; height: 600px;"
+        ></yandex-map>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { yandexMap } from 'vue-yandex-maps'
+export default {
+  components: {
+    yandexMap
+  },
+  data () {
+    return {
+      settings: {
+        API: 'f7da3df2-99ce-456f-b9e5-bc1934a8579a'
+      },
+      coords: [55.45, 36.37]
+    }
+  }
+}
+</script>
